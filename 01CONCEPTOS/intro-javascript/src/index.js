db28f4d715605729e8363d83073bced9f54d6e44
saulsistemas <santamariaramos18@hotmail.com>
2023-10-16 18:44:30 -0500

    JS - Repaso arreglos

--- 01CONCEPTOS/intro-javascript/src/index.js
+++ 01CONCEPTOS/intro-javascript/src/index.js
@@ -35,3 +35,25 @@ const persona2 = {
 persona2.nombre = 'Cesar';
 console.log(persona2);
 
+console.clear();
+console.log('=============Arreglos=======================');
+
+//const arreglos = new Array();
+const arreglos =  [];
+
+//push modifica el objeto princiapl
+arreglos.push(1)
+arreglos.push(2)
+arreglos.push(3)
+console.log(arreglos);
+
+const arreglos2 = [1,2,3,4]
+console.log(arreglos2);
+//... manda cada uno de los elementos
+const arreglos3 = [...arreglos2,5]
+console.log(arreglos3);
+
+const arreglos4 = arreglos3.map((numero)=>{
+    return numero*2
+});
+console.log(arreglos4);
