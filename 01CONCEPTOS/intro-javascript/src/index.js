f6ad143e3284a8c504f03c4d128444a8854477df
saulsistemas <santamariaramos18@hotmail.com>
2023-10-16 18:25:42 -0500

    JS - Repaso variables

---
+++ 01CONCEPTOS/intro-javascript/src/index.js
@@ -0,0 +1,15 @@
+//Variables
+
+const nombre = 'Carlos';
+const apellido = 'Rojas'
+let nombre2 = 'Miguel';
+nombre2= 'Pedro'
+console.log(nombre,apellido,nombre2);
+if(true){
+    const nombre = 'Calitos'
+    let nombre2 = 'Pedrito'
+    console.log(nombre);
+    console.log(nombre2);
+}
+console.log(nombre);
+console.log(nombre2);
