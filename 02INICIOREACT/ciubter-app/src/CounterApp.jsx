fc320028d7b7d557e4446d77409dc9cb0472cb5d
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 14:20:40 -0500

    hooks 2 q :

--- 02INICIOREACT/ciubter-app/src/CounterApp.jsx
+++ 02INICIOREACT/ciubter-app/src/CounterApp.jsx
@@ -6,19 +6,21 @@ export function CounterApp({value}) {
     const [counter,setCounter] = useState(value)
 
 
-
-    function handleAdd(event){
+    function handleAdd(){
         setCounter(counter+1)
     };
+    const handleAddSubstract= () => setCounter(counter-1)
+    const handleReset= () => setCounter(value)
+    
 
     return (
         <>
         <h1>Counter App</h1>
         <h2> { counter } </h2>
 
-        <button onClick={ (e)=>handleAdd(e) }>
-            +1
-        </button>
+        <button onClick={ (e)=>handleAdd(e) }>+1</button>
+        <button onClick={ handleAddSubstract }>-1</button>
+        <button onClick={ handleReset }>Reset</button>
         </>
     )
 }
