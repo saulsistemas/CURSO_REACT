c09942149246062a11e8bf0847a227262070d2f5
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 17:15:22 -0500

    iniciando aplicacion de gif

---
+++ 03APLICACIONGIF/gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+export const GifExpertApp =()=>{
+    return(
+        <>
+        {/*Titulo*/}
+        <h1>Hola este es mi primer componente</h1>
+        {/*Input*/}
+        
+        {/*Listado Gif*/}
+
+        </>
+    )
+}
