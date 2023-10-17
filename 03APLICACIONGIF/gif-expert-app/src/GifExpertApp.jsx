863d1324f018c0a283ab58f89f43f52d95f2538a
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 18:23:36 -0500

    crear componente para agregar categorias y eventos

--- 03APLICACIONGIF/gif-expert-app/src/GifExpertApp.jsx
+++ 03APLICACIONGIF/gif-expert-app/src/GifExpertApp.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp =()=>{
     const [categories, setCategories] = useState(['One Punch','Naruto'])
@@ -14,7 +15,7 @@ export const GifExpertApp =()=>{
         {/*Titulo*/}
         <h1>Hola este es mi primer componente</h1>
         {/*Input*/}
-        
+        <AddCategory></AddCategory>
         {/*Listado Gif*/}
         <button onClick={onAddCategories}>Agregar </button>
         <ol>
