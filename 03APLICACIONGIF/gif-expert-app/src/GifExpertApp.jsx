59075cc343c0434f703b85914917a2ba41ff664c
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 17:43:03 -0500

    creando listado de categorias

--- 03APLICACIONGIF/gif-expert-app/src/GifExpertApp.jsx
+++ 03APLICACIONGIF/gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+
 export const GifExpertApp =()=>{
+    const [categories, setCategories] = useState(['One Punch','Naruto'])
+    console.log(categories);
     return(
         <>
         {/*Titulo*/}
         <h1>Hola este es mi primer componente</h1>
         {/*Input*/}
-        
+        <ol>
+            { 
+                categories.map((category,indice)=>{
+                    return <li key={ indice }>{category}</li>
+                })
+            }
+            
+        </ol>
         {/*Listado Gif*/}
 
         </>
