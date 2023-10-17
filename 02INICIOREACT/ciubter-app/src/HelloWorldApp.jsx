3c5fcca89a6993ebc97a59b9ba54a4f3a8a7dafa
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 10:40:31 -0500

    impresion de variables JS en el html

--- 02INICIOREACT/ciubter-app/src/HelloWorldApp.jsx
+++ 02INICIOREACT/ciubter-app/src/HelloWorldApp.jsx
@@ -1,4 +1,20 @@
+const arrayNumero = [1,2,3,4,5,6,7,8,9];
+const objetoMensaje = {
+    nombre:"Juan",
+    Apellido: "Rojas"
+}
+
+const suma = (a,b)=> a+b;
+
 export function App(){
-    return (<h1>Hola mundo!!!!</h1>)
+
+    return (
+        <>
+            <h1>Esto es un Array  { arrayNumero }</h1>
+            <h1>Esto es un objeto { objetoMensaje.nombre}</h1>
+            <h1>Esto es un objeto { JSON.stringify(objetoMensaje)}</h1>
+            <h1>esto es una funcion de suma { suma(10,8)}</h1>
+        </>
+    )
 }
 
