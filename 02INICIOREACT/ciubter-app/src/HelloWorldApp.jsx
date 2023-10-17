5b1dfee07b2d247c6acaaeeb0d3da44c5d55349e
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 11:30:16 -0500

    props, comunicacion entre componente hijo y padre

--- 02INICIOREACT/ciubter-app/src/HelloWorldApp.jsx
+++ 02INICIOREACT/ciubter-app/src/HelloWorldApp.jsx
@@ -1,19 +1,9 @@
-const arrayNumero = [1,2,3,4,5,6,7,8,9];
-const objetoMensaje = {
-    nombre:"Juan",
-    Apellido: "Rojas"
-}
 
-const suma = (a,b)=> a+b;
-
-export function App(){
-
+export function App({title = 'Iroma'}){
     return (
         <>
-            <h1>Esto es un Array  { arrayNumero }</h1>
-            <h1>Esto es un objeto { objetoMensaje.nombre}</h1>
-            <h1>Esto es un objeto { JSON.stringify(objetoMensaje)}</h1>
-            <h1>esto es una funcion de suma { suma(10,8)}</h1>
+            <h1>Esto es un Heroe de marvel { title }</h1>
+           
         </>
     )
 }
--- 02INICIOREACT/ciubter-app/src/main.jsx
+++ 02INICIOREACT/ciubter-app/src/main.jsx
@@ -8,6 +8,6 @@ import './style.css'
 //3renderizacion
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <App title='Hola Soy goku' subTitle='Hola soy kakaroto'/>
     </React.StrictMode>
 )
