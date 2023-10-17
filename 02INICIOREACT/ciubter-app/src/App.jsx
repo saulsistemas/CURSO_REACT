c7b3cccdbbdc7a55b119b24109b11f1d4029a8dc
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 11:52:37 -0500

    DefaulProps

--- 02INICIOREACT/ciubter-app/src/App.jsx
+++ 02INICIOREACT/ciubter-app/src/App.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
-export function App({ title,subtitle }){
+export function App({ title = "Hulk",subtitle="Naruto",name }){
+    console.log(name);
     return (
         <>
             <h1>Esto es un Heroe de marvel { title }</h1>
             <h1>Esto es un Heroe de marvel { subtitle }</h1>
+            <h1>Esto es un Heroe de marvel { name }</h1>
            
         </>
     )
@@ -12,4 +14,10 @@ export function App({ title,subtitle }){
 App.propTypes= {
     title: PropTypes.string.isRequired,
     subtitle:PropTypes.number
+}
+
+App.defaultProps = {
+    title:"No hay titulo",
+    subtitle:123,
+    name:"ggg"
 }
