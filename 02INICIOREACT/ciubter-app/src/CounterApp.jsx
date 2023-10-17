2625aea436cccd0cf87d1072eeb485f6c2f39396
saulsistemas <santamariaramos18@hotmail.com>
2023-10-17 12:21:53 -0500

    evento click , eventos en general

--- 02INICIOREACT/ciubter-app/src/CounterApp.jsx
+++ 02INICIOREACT/ciubter-app/src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
 
 export function CounterApp({value}) {
+    function handleAdd(event){console.log(event)};
     return (
         <>
         <h1>Counter App</h1>
-        <h2> {value} </h2>
+        <h2> { value } </h2>
+
+        <button onClick={ (e)=>handleAdd(e) }>
+            +1
+        </button>
         </>
     )
 }
