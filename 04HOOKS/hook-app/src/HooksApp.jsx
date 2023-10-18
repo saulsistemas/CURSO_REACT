f94d17f2330b89f7fd6c1681629f7592887a90ed
saulsistemas <santamariaramos18@hotmail.com>
2023-10-18 17:57:46 -0500

    iniciando proyecto react hooks

--- 04HOOKS/hook-app/src/HooksApp.jsx
+++ 04HOOKS/hook-app/src/HooksApp.jsx
@@ -1,8 +1,11 @@
+import { CounterApp } from "./01-useState/CounterApp"
 
 export const HooksApp = () =>{
     return(
         <>
         <h1>Hooks App</h1>
+        <hr />
+        <CounterApp/>
         </>
     )
 }
