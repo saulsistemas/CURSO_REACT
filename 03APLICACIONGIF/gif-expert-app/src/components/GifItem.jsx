4abcf6a5b8b4069b6ff02ae44f8b7bf347ea91d8
saulsistemas <santamariaramos18@hotmail.com>
2023-10-18 17:08:59 -0500

    agregando proptypes

--- 03APLICACIONGIF/gif-expert-app/src/components/GifItem.jsx
+++ 03APLICACIONGIF/gif-expert-app/src/components/GifItem.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types'
 export const GifItem = ({title,url,id}) => {
     //console.log(title);
   return (
@@ -7,4 +7,8 @@ export const GifItem = ({title,url,id}) => {
         <p>{title}</p>
     </div>
   )
+}
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
