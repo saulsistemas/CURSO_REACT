3f247f3107e21c15da028d2c640002e644b45d9d
saulsistemas <santamariaramos18@hotmail.com>
2023-10-19 18:08:54 -0500

    hooks useEffect ejemplo 2

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/02-useEfect/MiPrimerEfecto.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/02-useEfect/MiPrimerEfecto.jsx
@@ -12,31 +12,35 @@ export const MiPrimerEfecto = () => {
     const modFecha=()=>{
         setFecha(Date.now())
     }
+
+    const [contador,setContador]=useState(0);
+
     // useEffect es un gancho que le permite realizar efectos secundarios en un componente funcional.
     //Solo se ejecuta una vez cuando colocas []
     useEffect(()=>{
         console.log('has cambiado');
     },[])
     useEffect(()=>{
-        console.log('has modificado el usuario');
-    },[usuario])
+        setContador(contador+1)
+        console.log('has modificado el usuario '+contador);
+    },[fecha,usuario])
 
     return (
         <div className="container">
-        <h3 className="label">Mi primer efecto: Hooks useEffect </h3>
-        <h5>{usuario}</h5>
-        <p>
+        <h3>Mi primer efecto: Hooks useEffect </h3>
+        <h5 className={contador>=10?"badge bg-success":"badge bg-primary"}>{usuario}</h5>
+        
             <input 
             type="text"
             className="form-control"
             onChange={modUsuario}
             />
-        <h5>{fecha}</h5>
+        <h5 className={contador>=10?"badge bg-success":"badge bg-primary"}>{fecha}</h5>
             <button
             onClick={modFecha}
             className="btn btn-primary"
             >Cambiar fecha</button>
-        </p>
+        
 
         </div>
     )
