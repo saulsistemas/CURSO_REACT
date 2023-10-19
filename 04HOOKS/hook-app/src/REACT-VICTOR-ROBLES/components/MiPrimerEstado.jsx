2d7539d9bb0dd1856c119bc1f975322046a22ccf
saulsistemas <santamariaramos18@hotmail.com>
2023-10-19 17:27:53 -0500

    hooks useState ejemplo2

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/components/MiPrimerEstado.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/components/MiPrimerEstado.jsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 
 export const MiPrimerEstado=()=>{
     //let nombre = "HelpNet";
-    
+
     const[nombre,setNombre]=useState("Help net")
-    const cambiarNombre=(e)=>{
-        setNombre('carlitos')
+
+    const cambiarNombre=(e,nombre)=>{
+        setNombre(nombre)
     }
 
     return(
         <>
-        <h3>Mi primer estado</h3>
+        <h3>Mi primer estado: Hooks useState </h3>
         <h5>{nombre}</h5>
-        <button onClick={cambiarNombre} className="btn btn-primary">Click para cambiar el nombre</button>
+        <input 
+        type="text" 
+        name="txtname"
+        className="form-control"
+        onChange={(e)=>{cambiarNombre(e,e.target.value)}}
+        />
+        <button onClick={(e)=>{cambiarNombre(e,"Help Net Boton")}} className="btn btn-primary">Click para cambiar el nombre</button>
         </>
     )
 }
