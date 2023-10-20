47be6abb26c94bc0ede2b585ec545ea331d66d44
saulsistemas <santamariaramos18@hotmail.com>
2023-10-19 21:41:57 -0500

    efecto cargar

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/03-Ajax/MiPrimerAjax.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/03-Ajax/MiPrimerAjax.jsx
@@ -5,31 +5,31 @@ export const MiPrimerAjax =()=>{
 
     const [usuarios,setUsuarios] = useState([])
     const [cargando,setCargando] = useState(true)
-    const getUSuarios = ()=>{
-        setUsuarios([
-            {
-            "id": 1,
-            "email": "michael.lawson@reqres.in",
-            "first_name": "Michael",
-            "last_name": "Lawson",
-            "avatar": "https://reqres.in/img/faces/7-image.jpg"
-            },
-            {
-            "id": 2,
-            "email": "lindsay.ferguson@reqres.in",
-            "first_name": "Lindsay",
-            "last_name": "Ferguson",
-            "avatar": "https://reqres.in/img/faces/8-image.jpg"
-            },
-            {
-            "id": 3,
-            "email": "tobias.funke@reqres.in",
-            "first_name": "Tobias",
-            "last_name": "Funke",
-            "avatar": "https://reqres.in/img/faces/9-image.jpg"
-            },
-        ])
-    }
+    //const getUSuarios = ()=>{
+    //    setUsuarios([
+    //        {
+    //        "id": 1,
+    //        "email": "michael.lawson@reqres.in",
+    //        "first_name": "Michael",
+    //        "last_name": "Lawson",
+    //        "avatar": "https://reqres.in/img/faces/7-image.jpg"
+    //        },
+    //        {
+    //        "id": 2,
+    //        "email": "lindsay.ferguson@reqres.in",
+    //        "first_name": "Lindsay",
+    //        "last_name": "Ferguson",
+    //        "avatar": "https://reqres.in/img/faces/8-image.jpg"
+    //        },
+    //        {
+    //        "id": 3,
+    //        "email": "tobias.funke@reqres.in",
+    //        "first_name": "Tobias",
+    //        "last_name": "Funke",
+    //        "avatar": "https://reqres.in/img/faces/9-image.jpg"
+    //        },
+    //    ])
+    //}
     //const getUsuariosAjaxFetch = ()=>{
     //    fetch('https://reqres.in/api/users?page=1')
     //        .then(response => response.json())
@@ -42,12 +42,17 @@ export const MiPrimerAjax =()=>{
     //}
 
     const getUsuarioAjaxAwait = async () =>{
-        const url= 'https://reqres.in/api/users?page=1'
-        const respuesta = await fetch(url);
-        const {data} = await respuesta.json();
-        setUsuarios(data)
-        setCargando(false)
-        console.log(data);
+        try {
+            const url= 'https://reqres.in/api/users?page=1'
+            const respuesta = await fetch(url);
+            const {data} = await respuesta.json();
+            setUsuarios(data)
+            setCargando(false)
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     useEffect(() => {
