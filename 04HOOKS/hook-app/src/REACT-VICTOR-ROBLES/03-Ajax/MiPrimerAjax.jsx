2d1756206b36f1873560942d3fda1db0ff9b73b2
saulsistemas <santamariaramos18@hotmail.com>
2023-10-19 21:28:41 -0500

    ajax listado con async await

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/03-Ajax/MiPrimerAjax.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/03-Ajax/MiPrimerAjax.jsx
@@ -29,31 +29,47 @@ export const MiPrimerAjax =()=>{
             },
         ])
     }
-    const getUsuariosAjax = ()=>{
-        fetch('https://reqres.in/api/users?page=1')
-            .then(response => response.json())
-            .then(resultado_final => {
-                setUsuarios(resultado_final.data)
-                console.log(resultado_final.data)
-            },
-            error => console.log(error)
-            )
+    //const getUsuariosAjaxFetch = ()=>{
+    //    fetch('https://reqres.in/api/users?page=1')
+    //        .then(response => response.json())
+    //        .then(resultado_final => {
+    //            setUsuarios(resultado_final.data)
+    //            //console.log(resultado_final.data)
+    //        },
+    //        error => console.log(error)
+    //        )
+    //}
+
+    const getUsuarioAjaxAwait = async () =>{
+        const url= 'https://reqres.in/api/users?page=1'
+        const respuesta = await fetch(url);
+        const {data} = await respuesta.json();
+        setUsuarios(data)
+        console.log(data);
     }
+
     useEffect(() => {
-        getUsuariosAjax();
+        getUsuarioAjaxAwait();
     }, [])
-    //console.log(getUsuariosAjax());
+    //console.log(getUsuarioAjaxAwait());
     return(
         <div className="container">
             <h3>Mi primer Ajax: Hooks Ajax </h3>
-            <ol>
+            <div className="row">
                 {
-
-                    usuarios.map(({id,first_name,last_name ,email})=>{
-                        return <li key={id}>{first_name}</li>
+                    usuarios.map(({id,first_name,last_name ,email,avatar})=>{
+                       return   <div className="col-sm-4" key={id}>
+                                <div className="card" >
+                                    <img src={avatar} className="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{first_name}</h5>
+                                        <p className="card-text"> {last_name} | {email} </p>
+                                    </div>
+                                </div>
+                                </div>
                     })
                 }
-            </ol>
+            </div>
         </div>
     )
 }
