adcea287bd6f962507a11d52eabe28de3bbb2c4c
saulsistemas <santamariaramos18@hotmail.com>
2023-10-24 00:11:29 -0500

    Capturar datos de formulario

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/05-projecto-peliculas/componentes/Agregar.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/05-projecto-peliculas/componentes/Agregar.jsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
 
 export const Agregar = () => {
+    const tituloComponente = "Añadir Pelicula";
+
+    const [pelicula,setPeliculas] = useState({});
+
+    //Desestructuracion del objeto
+    const {titulo,descripcion} = pelicula;
+
+    const conseguirDatosForm=function(e){
+        e.preventDefault();
+        
+        // 1) conseguir datos del formulario
+        const target    = e.target;
+        const titulo      = target.titulo.value;
+        const descripcion = target.descripcion.value;
+
+        // 02)crear objeta de la peliucula
+        const pelicula = {
+            id: new Date().getTime(),
+            titulo,
+            descripcion
+        }
+        setPeliculas(pelicula);
+    }
+    
+    
     return (
         <>
-            <form action="">
-                <input type="text" className="form-control" name="titulo" /><br />
-                <textarea name="titulo" className="form-control"></textarea><br />
-                <input className="btn btn-primary" type="submit" value="Enviar" />
+            <h5>{ tituloComponente }</h5>
+            {(titulo && descripcion) && " Has creado la pelicula "+titulo}
+            <form onSubmit={conseguirDatosForm}>
+                <input type="text" 
+                name="titulo" 
+                className="form-control" 
+                /><br />
+
+                <textarea 
+                name="descripcion" 
+                className="form-control"
+                ></textarea><br />
+
+                <input 
+                className="btn btn-primary" 
+                type="submit" 
+                value="Enviar" />
+
             </form>
         </>
     )
