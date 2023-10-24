3393fb562b67075f5e42ed921aa7e7294a48f36e
saulsistemas <santamariaramos18@hotmail.com>
2023-10-24 17:18:52 -0500

    Creando menu con NavLink

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/06-rutas/routers/RouterPrincipal.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/06-rutas/routers/RouterPrincipal.jsx
@@ -1,19 +1,44 @@
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, NavLink, BrowserRouter } from 'react-router-dom'
 import { Inicio } from '../componentes/inicio'
 import { Contacto } from '../componentes/Contacto'
 import { Articulo } from '../componentes/Articulo'
 
 export const RouterPrincipal = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Inicio/>} />
-            <Route path='/inicio' element={<Inicio/>} />
-            <Route path='/articulo' element={<Articulo/>} />
-            <Route path='/contacto' element={<Contacto/>} />
-            <Route path='*' element={<h1> Error | 404 </h1> } />
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
 
-        </Routes>
-    </BrowserRouter>
-  )
+                    <div className="container-fluid">
+                        <NavLink to='/inicio' className="navbar-brand" >Mis Peliculas</NavLink>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <NavLink className="nav-link active" aria-current="page" to='/inicio' >Inicio</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to='/articulo'>Artículos</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to='/contacto'>Contacto</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <Routes>
+                    <Route path='/' element={<Inicio />} />
+                    <Route path='/inicio' element={<Inicio />} />
+                    <Route path='/articulo' element={<Articulo />} />
+                    <Route path='/contacto' element={<Contacto />} />
+                    <Route path='*' element={<h1> Error | 404 </h1>} />
+
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
 }
