ad709570e0a1e8b05adf24b57f94bf712cb33322
saulsistemas <santamariaramos18@hotmail.com>
2023-10-24 00:30:11 -0500

    Agegando Peliculas al localStorage

--- 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/05-projecto-peliculas/componentes/Agregar.jsx
+++ 04HOOKS/hook-app/src/REACT-VICTOR-ROBLES/05-projecto-peliculas/componentes/Agregar.jsx
@@ -23,8 +23,29 @@ export const Agregar = () => {
             descripcion
         }
         setPeliculas(pelicula);
+        //Guardar en el localStorage
+        guardarEnLocalStorage(pelicula);
+
+        
     }
     
+    const guardarEnLocalStorage= function (pelicula){
+        //Conseguir los elementos que ya tenemos en el localStorage
+
+        let elementos = JSON.parse(localStorage.getItem("pelis"))
+
+        //comprobar si es un array
+        if (Array.isArray(elementos)) {
+            //agregar objeto nuevo
+            elementos.push(pelicula);
+        }else{
+            elementos = [pelicula]
+        }
+        //Guardar en el local storage
+        localStorage.setItem('pelis',JSON.stringify(elementos))
+        console.log(elementos);
+        return pelicula;
+    }
     
     return (
         <>
